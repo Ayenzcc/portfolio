405b8f1ac9c96a0e101be0014e2e9de2ac4d78e3
Ayenzcc <rodcotines@gmail.com>
2023-07-26 21:22:22 +0800

    change typeanimtion strings

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,9 +34,9 @@ const Hero = () => {
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                "Web Developer",
+                "UI/UX Designer",
                 2000, // wait 1s before replacing "Mice" with "Hamsters"
-                "Software Engr.",
+                "Web Developer",
                 2000,
               ]}
               wrapper="span"
