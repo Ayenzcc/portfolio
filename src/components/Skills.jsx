b0bc07aab89c05f55f4bf6f3102e78d854b48a19
Ayenzcc <rodcotines@gmail.com>
2023-07-30 19:36:03 +0800

    added constants arr [] of objects

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,7 @@ import PS from "/PS.png";
 import WEB from "/web.webp";
 import Prog from "/Prog.webp";
 import { motion } from "framer-motion";
+import { skillsSet } from "../constants";
 
 const Skills = () => {
   return (
@@ -26,153 +27,29 @@ const Skills = () => {
         </div>
         <div className="w-[100%] "> </div>
         <div className="w-[100%] "> </div>
-
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 0.3 }}
-          whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
-          className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
-        >
-          <img
-            className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
-            src={Rendered}
-            alt="/"
-          />
-
-          <motion.h2
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 0.3 }}
-            className=" text-white text-2xl font-bold text-center py-8"
-          >
-            Blender
-          </motion.h2>
-          <h2 className=" text-center text-white font-medium text-base">
-            3d Modeling | Isometric Design | Animation
-          </h2>
-        </motion.div>
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 0.6 }}
-          whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
-          className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
-        >
-          <img
-            className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
-            src={PS}
-            alt="/"
-          />
-
-          <motion.h2
+        {skillsSet.map((skills, i) => (
+          <motion.div
             initial={{ x: -50, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ ease: "easeOut", duration: 0.6 }}
-            className=" text-white text-2xl font-bold text-center py-8"
+            whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
+            className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
+            key={i}
           >
-            Adobe Photoshop
-          </motion.h2>
-          <h2 className=" text-center text-white font-medium text-base">
-            Photo Editing | Image Creation | Graphic Layout Design
-          </h2>
-        </motion.div>
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 0.9 }}
-          whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
-          className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
-        >
-          <img
-            className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
-            src={Godot}
-            alt="/"
-          />
-          <motion.h2
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 0.9 }}
-            className=" text-white text-2xl font-bold text-center py-8"
-          >
-            Godot
-          </motion.h2>
-          <h2 className=" text-center text-white font-medium text-base">
-            Simple 2d & 3d Game Development
-          </h2>
-        </motion.div>
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 1.2 }}
-          whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
-          className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
-        >
-          <img
-            className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
-            src={PR}
-            alt="/"
-          />
-          <motion.h2
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1.2 }}
-            className=" text-white text-2xl font-bold text-center py-8"
-          >
-            Adobe Premiere Pro
-          </motion.h2>
-          <h2 className=" text-center text-white font-medium text-base">
-            Video Editing
-          </h2>
-        </motion.div>
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 1.5 }}
-          whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
-          className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
-        >
-          <img
-            className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
-            src={WEB}
-            alt="/"
-          />
-          <motion.h2
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1.5 }}
-            className=" text-white text-2xl font-bold text-center py-8"
-          >
-            Web Development
-          </motion.h2>
-          <h2 className=" text-center text-white font-medium text-base">
-            HTML | CSS | JavaScript | MySQL | WordPress | React | Tailwind
-          </h2>
-        </motion.div>
-        <motion.div
-          initial={{ x: -50, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ ease: "easeOut", duration: 1.8 }}
-          whileHover={{ scale: "1.05", opacity: "3", duration: 1 }}
-          className="w-full h-64 shadow-md shadow-[#00df9a] flex flex-col p-4 my-4 "
-        >
-          <img
-            className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
-            src={Prog}
-            alt="/"
-          />
-          <motion.h2
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1.8 }}
-            className=" text-white text-2xl font-bold text-center py-8"
-          >
-            Programming
-          </motion.h2>
-          <h2 className=" text-center text-white font-medium text-base">
-            Java | Python | JavaScript
-          </h2>
-        </motion.div>
+            <img
+              className="w-20 h-20 mx-auto mt-[-3rem] rounded-md "
+              src={skills.image}
+              alt="/"
+            />
+
+            <h2 className=" text-white text-2xl font-bold text-center py-8">
+              {skills.text}
+            </h2>
+            <h2 className=" text-center text-white font-medium text-base">
+              {skills.text1}
+            </h2>
+          </motion.div>
+        ))}
       </motion.div>
     </div>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,46 @@
+export const skillsSet = [
+  {
+    image: "/blender.png",
+
+    text: "Blender",
+
+    text1: "3d Modeling | Isometric Design | Animation",
+  },
+
+  {
+    image: "/PS.png",
+
+    text: "Photoshop",
+
+    text1: "Photo Editing | Image Creation | Graphic Layout Design",
+  },
+  {
+    image: "/Godottt.png",
+
+    text: "Godot",
+
+    text1: "Simple 2d & 3d Game Development",
+  },
+  {
+    image: "/PR.png",
+
+    text: "Adobe Premiere Pro",
+
+    text1: "Video Editing",
+  },
+
+  {
+    image: "/web.webp",
+
+    text: "Web Development",
+
+    text1: "HTML | CSS | JavaScript | MySQL | WordPress | React | Tailwind CSS",
+  },
+  {
+    image: "/Prog.webp",
+
+    text: "Programming",
+
+    text1: "Java | Python | JavaScript",
+  },
+];
